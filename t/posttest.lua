65e11a9f6115636a7220719091f2a741d791283a
baina <baina@189.cn>
2014-03-20 01:21:36 +0800

    client_header_buffer_size
    client_header_buffer_size
    client_body_buffer_size
    
          client_max_body_size 1024k;
          client_body_buffer_size 1024k;
    
    http://rhomobi.com/topics/204

--- t/posttest.lua
+++ t/posttest.lua
@@ -41,9 +41,8 @@ end
 -- http://yougola.sinaapp.com/checker/?/intl/ctrip/20131201.20131231/shalon
 -- http://yougola.sinaapp.com/checker/?intl/ctrip/20131130.20131230/bjslon
 local sinaapp = false;
--- local baseurl = "http://yougola.sinaapp.com/";
-local baseurl = "http://bcc.bestfly.cn/";
-local md5uri = "checker/?intl/ctrip/20131130.20131230/bjslon";
+local baseurl = "http://cloudavh.sinaapp.com/";
+local md5uri = "checker/?intl/kayak/20131130.20131230/bjslon";
 local sinakey = "5P826n55x3LkwK5k88S5b3XS4h30bTRg";
 local timestamp = os.time() + 3600;
 print(timestamp);
@@ -53,69 +52,29 @@ print("--------------")
 print(md5.sumhexa("BJS0650/DXB1150-DXB1445/LON1825,LON1335/DXB0035-DXB0335/BJS1445"))
 -- init response table
 local respbody = {};
--- local body, code, headers = http.request(baseurl .. md5uri)
-local body, code, headers, status = http.request {
--- local ok, code, headers, status, body = http.request {
-	-- url = "http://cloudavh.com/data-gw/index.php",
-	-- url = "http://yougola.sinaapp.com/tools/proxy2/?url=" .. urlencode(baseurl .. md5uri),
-	url = baseurl .. md5uri,
-	-- proxy = "http://112.124.211.29:18085",
-	-- proxy = "http://" .. tostring(arg[2]),
-	timeout = 1,
-	method = "GET", -- POST or GET
-	-- add post content-type and cookie
-	-- headers = { ["Content-Type"] = "application/x-www-form-urlencoded", ["Content-Length"] = string.len(form_data) },
-	-- headers = { ["Host"] = "flight.itour.cn", ["X-AjaxPro-Method"] = "GetFlight", ["Cache-Control"] = "no-cache", ["Accept-Encoding"] = "gzip,deflate,sdch", ["Accept"] = "*/*", ["Origin"] = "chrome-extension://fdmmgilgnpjigdojojpjoooidkmcomcm", ["Connection"] = "keep-alive", ["Content-Type"] = "application/json", ["Content-Length"] = string.len(JSON.encode(request)), ["User-Agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.65 Safari/537.36" },
-	headers = {
-		-- ["Host"] = "yougola.sinaapp.com",
-		-- ["SOAPAction"] = "http://ctrip.com/Request",
-		["Cache-Control"] = "no-cache",
-		["Auth-Timestamp"] = timestamp,
-		["Auth-Signature"] = md5.sumhexa(sinakey .. timestamp),
-		-- ["Accept-Encoding"] = "gzip",
-		-- ["Accept"] = "*/*",
-		["Connection"] = "keep-alive",
-		-- ["Content-Type"] = "text/xml; charset=utf-8",
-		-- ["Content-Length"] = string.len(request)
-	},
-	-- body = formdata,
-	-- source = ltn12.source.string(form_data);
-	-- source = ltn12.source.string(request),
-	sink = ltn12.sink.table(respbody)
-}
-print(code, status, body)
---[[
-for k, v in pairs(headers) do
-	print(k, v)
-end
---]]
+-- local wname = "C:\\Lua\\5.1\\changes.txt"
+local wname = "/data/logs/ngxlua.html"
+local wfile = io.open(wname, "r");
+local request = wfile:read("*a");
 print("--------------")
-print(body)
+print(string.len(request));
 print("--------------")
-print(status)
-print("--------------")
-local resjson = "";
-local reslen = table.getn(respbody)
-print(reslen)
-for i = 1, reslen do
-	-- print(respbody[i])
-	resjson = resjson .. respbody[i]
-end
-print(resjson)
-print("--------------")
--- init response table
-local respbody = {};
-local request = md5.sumhexa(resjson) .. timestamp;
+-- request = md5.sumhexa(request) .. timestamp .. string.len(request);
+-- request = base64.encode(request);
+local form_data = string.sub(request, 1, -1) .. "8"
 print(request);
-print("-------开始发送POST请求-------")
+print("--------------")
+print("-------寮�濮嬪彂閫丳OST璇锋眰-------")
 -- local body, code, headers = http.request(baseurl .. md5uri)
 local body, code, headers, status = http.request {
 -- local ok, code, headers, status, body = http.request {
 	-- url = "http://cloudavh.com/data-gw/index.php",
-	url = baseurl .. md5uri,
+	-- url = baseurl .. md5uri,
+	-- url = "http://rhomobi.com:18081/rholog",
+	url = "http://172.16.30.1:3000/citycns",
 	-- proxy = "http://112.124.211.29:18085",
 	-- proxy = "http://" .. tostring(arg[2]),
-	timeout = 1,
+	timeout = 3000,
 	method = "POST", -- POST or GET
 	-- add post content-type and cookie
 	-- headers = { ["Content-Type"] = "application/x-www-form-urlencoded", ["Content-Length"] = string.len(form_data) },
@@ -130,11 +89,11 @@ local body, code, headers, status = http.request {
 		-- ["Accept"] = "*/*",
 		["Connection"] = "keep-alive",
 		-- ["Content-Type"] = "text/xml; charset=utf-8",
-		["Content-Length"] = string.len(request)
+		["Content-Length"] = string.len(form_data)
 	},
 	-- body = formdata,
-	-- source = ltn12.source.string(form_data);
-	source = ltn12.source.string(request),
+	source = ltn12.source.string(form_data);
+	-- source = ltn12.source.string(request),
 	sink = ltn12.sink.table(respbody)
 }
 print(code, status, body)
